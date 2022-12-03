7ac9f29aa12e803e466c872875c586f14d6c91aa
Hassan H. Hassan <13298351+Ulq15@users.noreply.github.com>
2022-12-03 13:28:06 -0800

    Update agents.py

--- agents.py
+++ agents.py
@@ -3,6 +3,8 @@ from pysc2.lib.actions import FUNCTIONS, FunctionCall
 from pysc2.lib import features
 from pysc2.lib.units import Terran, Neutral
 
+import numpy
+
 from time import sleep
 
 
@@ -13,6 +15,7 @@ _BUILD_REFINERY = FUNCTIONS.Build_Refinery_screen.id
 _TRAIN_MARINE = FUNCTIONS.Train_Marine_quick.id
 _NOOP = FUNCTIONS.no_op.id
 _SELECT_POINT = FUNCTIONS.select_point.id
+
 
 # Features (used as indexes to the obs.observation list)
 _PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index
@@ -25,10 +28,6 @@ _SCV = Terran.SCV
 _SUPPLYDEPOT = Terran.SupplyDepot
 _BARRACKS = Terran.Barracks
 _REFINERY = Terran.Refinery
-_BUILDINGS = {Terran.SupplyDepot:0,
-             Terran.Barracks:0,
-             Terran.Refinery:0}
-_VESPENE = [Neutral.VespeneGeyser, Neutral.RichVespeneGeyser, Neutral.ShakurasVespeneGeyser, Neutral.PurifierVespeneGeyser]
 
 # Parameters
 _PLAYER_SELF = features.PlayerRelative.SELF
@@ -36,24 +35,23 @@ _PLAYER_NEUTRAL = features.PlayerRelative.NEUTRAL  # beacon/minerals
 _PLAYER_ENEMY = features.PlayerRelative.ENEMY
 _NOT_QUEUED = [0]
 _QUEUED = [1]
-# _MAP_SIZE = (X,Y)
 
 
 class Agent(base_agent.BaseAgent):
 
     def __init__(self):
         super(Agent, self).__init__()
-    
+
     def setup(self, obs_spec, action_spec):
         super(Agent, self).setup(obs_spec, action_spec)
-        # self.locate_base()
-        self.buildings = _BUILDINGS.copy()
 
     def reset(self):
         super(Agent, self).reset()
+        self.base_top_left = None
         self.supply_depot_built = False
         self.barracks_built = False
         self.refinery_built = False
+        self.scv_selected = False
 
     # helper method to transform relative player view for 64x64 map
     def transformLocation(self, x, x_distance, y, y_distance):
@@ -61,14 +59,13 @@ class Agent(base_agent.BaseAgent):
             return [x - x_distance, y - y_distance]
         return [x + x_distance, y + y_distance]
 
-    # def locate_base(self):
-    #     # To determine location of player's base on map
-    #     # for 64x64 map, if player_x | player_y <= 64/2, then player base is top left of 64x64 map
-    #     # otherwise the player base is bottom right of 64x64 map
-    #     if self.base_top_left is None:
-    #         player_y, player_x = (self.obs_spec["feature_minimap"][_PLAYER_RELATIVE] == _PLAYER_SELF).nonzero()
-    #         self.base_top_left = player_y.mean() <= 31
-
+    def locate_base(self, obs):
+        # To determine location of player's base on map
+        # for 64x64 map, if player_x | player_y <= 64/2, then player base is top left of 64x64 map
+        # otherwise the player base is bottom right of 64x64 map
+        if self.base_top_left is None:
+            player_y, player_x = (obs.observation["feature_minimap"][_PLAYER_RELATIVE] == _PLAYER_SELF).nonzero()
+            self.base_top_left = player_y.mean() <= 31
 
 
 class BuildingAgent(Agent):
@@ -76,31 +73,35 @@ class BuildingAgent(Agent):
     def step(self, obs):
         super(BuildingAgent, self).step(obs)
         # sleep(1)
-        
-        if not self.supply_depot_built or not self.refinery_built or not self.barracks_built:
+
+        if self.base_top_left is None:
+            self.locate_base(obs)
+
+        if not self.scv_selected: #self.supply_depot_built or not self.refinery_built or not self.barracks_built:
             unit_type = obs.observation["feature_screen"][_UNIT_TYPE]
             unit_y, unit_x = (unit_type == _SCV).nonzero()
             target = [unit_x[0], unit_y[0]]
+            self.scv_selected = True
             return FunctionCall(_SELECT_POINT, [_NOT_QUEUED, target])
-        
+
         # Checkes if Supply depot has been built
         # if Supply depot has not been built, then select SCV unit and build it
-        if not self.supply_depot_built and _BUILD_SUPPLYDEPOT in obs.observation["available_actions"]:
+        elif not self.supply_depot_built and _BUILD_SUPPLYDEPOT in obs.observation["available_actions"]:
             unit_type = obs.observation["feature_screen"][_UNIT_TYPE]
             unit_y, unit_x = (unit_type == _COMMANDCENTER).nonzero()
             target = self.transformLocation(int(unit_x.mean()), 0, int(unit_y.mean()), 20)
             self.supply_depot_built = True
-            self.buildings[_SUPPLYDEPOT] += 1
+            self.scv_selected = False
             return FunctionCall(_BUILD_SUPPLYDEPOT, [_NOT_QUEUED, target])
-            
-        elif not self.refinery_built and _BUILD_REFINERY in obs.observation["available_actions"]:
-            unit_type = obs.observation["feature_screen"][_UNIT_TYPE]
-            unit_y, unit_x = (unit_type in _VESPENE).nonzero()
-            target = [unit_x[0], unit_y[0]]
-            self.refinery_built = True
-            self.buildings[_REFINERY] += 1
-            return FunctionCall(_BUILD_REFINERY, [_NOT_QUEUED, target])
-        
+
+        # elif not self.refinery_built and _BUILD_REFINERY in obs.observation["available_actions"]:
+        #     unit_type = obs.observation["feature_screen"][_UNIT_TYPE]
+        #     unit_y, unit_x = (unit_type == Neutral.VespeneGeyser).nonzero()
+        #     target = [int(unit_x.mean()), int(unit_y.mean())]
+        #     self.refinery_built = True
+        #     self.scv_selected = False
+        #     return FunctionCall(_BUILD_REFINERY, [_NOT_QUEUED, target])
+
         # Checkes if Barracks has been built
         # if Barracks has not been built, then select SCV unit and build it
         elif not self.barracks_built and _BUILD_BARRACKS in obs.observation["available_actions"]:
@@ -108,18 +109,8 @@ class BuildingAgent(Agent):
             unit_y, unit_x = (unit_type == _COMMANDCENTER).nonzero()
             target = self.transformLocation(int(unit_x.mean()), 20, int(unit_y.mean()), 0)
             self.barracks_built = True
-            self.buildings[_BARRACKS] += 1
             return FunctionCall(_BUILD_BARRACKS, [_NOT_QUEUED, target])
-            
-        return FunctionCall(_NOOP, [])
 
-
-
-class DefenceAgent(Agent):
-
-    def step(self, obs):
-        super(DefenceAgent, self).step(obs)
-        
         return FunctionCall(_NOOP, [])
 
 
@@ -128,5 +119,39 @@ class ArmyAgent(BuildingAgent):
 
     def step(self, obs):
         super(ArmyAgent, self).step(obs)
-        
+        if self.base_top_left is None:
+            self.locate_base(obs)
+
+        if not self.scv_selected: #self.supply_depot_built or not self.refinery_built or not self.barracks_built:
+            unit_type = obs.observation["feature_screen"][_UNIT_TYPE]
+            unit_y, unit_x = (unit_type == _SCV).nonzero()
+            target = [unit_x[0], unit_y[0]]
+            self.scv_selected = True
+            return FunctionCall(_SELECT_POINT, [_NOT_QUEUED, target])
+        elif not self.supply_depot_built and _BUILD_SUPPLYDEPOT in obs.observation["available_actions"]:
+            unit_type = obs.observation["feature_screen"][_UNIT_TYPE]
+            unit_y, unit_x = (unit_type == _COMMANDCENTER).nonzero()
+            target = self.transformLocation(int(unit_x.mean()), 0, int(unit_y.mean()), 20)
+            self.supply_depot_built = True
+            self.scv_selected = False
+            return FunctionCall(_BUILD_SUPPLYDEPOT, [_NOT_QUEUED, target])
+        elif not self.barracks_built and _BUILD_BARRACKS in obs.observation["available_actions"]:
+            unit_type = obs.observation["feature_screen"][_UNIT_TYPE]
+            unit_y, unit_x = (unit_type == _COMMANDCENTER).nonzero()
+            target = self.transformLocation(int(unit_x.mean()), 20, int(unit_y.mean()), 0)
+            self.barracks_built = True
+            return FunctionCall(_BUILD_BARRACKS, [_NOT_QUEUED, target])
+
+        if self.barracks_built == True:
+            return FunctionCall(_TRAIN_MARINE, [_NOT_QUEUED])
+
+        return FunctionCall(_NOOP, [])
+
+
+
+class DefenceAgent(ArmyAgent):
+
+    def step(self, obs):
+        super(DefenceAgent, self).step(obs)
+
         return FunctionCall(_NOOP, [])
